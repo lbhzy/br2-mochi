31f40e78de06004dd89d16d2c68cd257dcc78804
lbhzy <296384478z@gmail.com>
2026-01-22 21:41:44 +0800

    添加 run 目标一键运行 qemu

--- external.mk
+++ external.mk
@@ -1,1 +1,11 @@
 include $(sort $(wildcard $(BR2_EXTERNAL_MOCHI_PATH)/package/*/*.mk))
+
+run:
+	@echo -e "\033[33m[INFO] Press 'Ctrl+a' then 'x' to terminate QEMU\033[0m"
+	@qemu-system-aarch64 \
+		-machine virt \
+		-cpu cortex-a57 \
+		-nographic \
+		-kernel $(BINARIES_DIR)/Image \
+		-append "console=ttyAMA0 rdinit=/init" \
+		-initrd $(BINARIES_DIR)/rootfs.cpio
