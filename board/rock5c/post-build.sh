e23db269bb285bc7db47e4c93059f9df09b9bbf1
lbhzy <296384478z@gmail.com>
2026-08-22 13:24:32 +0800

    feat(rock5c): 新增独立 boot 分区

--- board/rock5c/post-build.sh
+++ board/rock5c/post-build.sh
@@ -2,4 +2,4 @@
 
 BOARD_DIR="$(dirname "$0")"
 
-install -m 0644 -D "$BOARD_DIR"/extlinux.conf "$TARGET_DIR"/boot/extlinux/extlinux.conf
+install -m 0644 -D "$BOARD_DIR"/extlinux.conf "$BINARIES_DIR"/extlinux.conf
